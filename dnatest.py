9773172d9b2772ae9f79b6a9fc841fd4a2193ee6
dsantoyo2018 <43888543+dsantoyo2018@users.noreply.github.com>
2023-02-16 14:02:34 -0500

    Update dnatest.py
    correcion de comillas simples

--- dnatest.py
+++ dnatest.py
@@ -1,6 +1,6 @@
 from dnacentersdk import api
 
-dnac = api.DNACenterAPI(base_url=’https://sandboxdnac2.cisco.com:443’,username=’devnetuser’,password=’Cisco123!’, verify=False)
+dnac = api.DNACenterAPI(base_url='https://sandboxdnac2.cisco.com:443',username='devnetuser',password='Cisco123!', verify=False)
 
 try:
 devices = dnac.devices.get_device_list()

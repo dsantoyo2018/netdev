ca12ad5108f5c5b2eec2caf6d603f08c2c59110d
dsantoyo2018 <43888543+dsantoyo2018@users.noreply.github.com>
2023-02-16 14:07:32 -0500

    Update dnatest.py
    estrcutura 4

--- dnatest.py
+++ dnatest.py
@@ -6,5 +6,5 @@ try:
   devices = dnac.devices.get_device_list()
   for device in devices.response:
     print(device['hostname']," ", device['managementIpAddress'])
-  except ApiError as e:
-    print(e)
+except ApiError as e:
+  print(e)

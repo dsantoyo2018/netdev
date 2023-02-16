f2eec91c215a15d4ef46dfb1f770d3f07e66bb54
dsantoyo2018 <43888543+dsantoyo2018@users.noreply.github.com>
2023-02-16 14:04:17 -0500

    Update dnatest.py
    correcion de estrcutura

--- dnatest.py
+++ dnatest.py
@@ -3,8 +3,9 @@ from dnacentersdk import api
 dnac = api.DNACenterAPI(base_url='https://sandboxdnac2.cisco.com:443',username='devnetuser',password='Cisco123!', verify=False)
 
 try:
-devices = dnac.devices.get_device_list()
+  devices = dnac.devices.get_device_list()
+
 for device in devices.response:
-print(device['hostname']," ", device['managementIpAddress'])
-except ApiError as e:
-print(e)
+ print(device['hostname']," ", device['managementIpAddress'])
+ except ApiError as e:
+ print(e)
